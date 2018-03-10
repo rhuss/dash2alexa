b0af81f7dbfb82b277a36aa8735dff13b8591e58
Roland Huß <roland@ro14nd.de>
2018-03-10 16:41:45 +0100

    fix: config files are now picked up properly

--- dash2alexa.go
+++ dash2alexa.go
@@ -55,13 +55,13 @@ func init() {
 }
 
 func initConfig() {
-	if cfgFile != "" { // enable ability to specify config file via flag
-		viper.SetConfigFile(cfgFile)
-	}
-
 	viper.SetConfigName(".dash2alexa") // name of config file (without extension)
 	viper.AddConfigPath("$HOME/")      // adding home directory as first search path
 	viper.AutomaticEnv()               // read in environment variables that match
+
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
